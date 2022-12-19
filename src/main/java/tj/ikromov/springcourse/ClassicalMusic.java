49137671d5c06c793dc51c492ea67a483b7fd701
Manuchehr Ikromov <manuchehr.ikromov01@gmail.com>
2022-12-19 17:18:03 +0500

    vtor_ur

---
+++ src/main/java/tj/ikromov/springcourse/ClassicalMusic.java
@@ -0,0 +1,8 @@
+package tj.ikromov.springcourse;
+
+public class ClassicalMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Hungarian Rhapsody";
+    }
+}
---
+++ src/main/java/tj/ikromov/springcourse/Music.java
@@ -0,0 +1,5 @@
+package tj.ikromov.springcourse;
+
+public interface Music {
+    String getSong();
+}

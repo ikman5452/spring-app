eb18ee66a1a161e9b203c840566ba392cefd36bb
Manuchehr Ikromov <manuchehr.ikromov01@gmail.com>
2022-12-22 16:43:40 +0500

    7ur_Tregulov

--- src/main/java/spring_introduction_tregulov/Person.java
+++ src/main/java/spring_introduction_tregulov/Person.java
@@ -1,10 +1,19 @@
 package spring_introduction_tregulov;
 
 public class Person {
-    private final Pet pet;
+    private Pet pet;
 
-    public Person(Pet pet) {
+//    public Person(Pet pet) {
+//        System.out.println("Person bean is created!");
+//        this.pet = pet;
+//    }
+
+    public Person() {
         System.out.println("Person bean is created!");
+    }
+
+    public void setPet(Pet pet) {
+        System.out.println("In class Person set pet!");
         this.pet = pet;
     }
 
